0ec93e052053656bb39ab5f2d9eed6bf7e0fd563
Angelo Fallaria <ba.fallaria@gmail.com>
2023-12-02 13:46:00 +0800

    style: format with gofumpt

--- frame.go
+++ frame.go
@@ -1,23 +1,23 @@
 package jewl
 
-//This is simply a type alias to make reading the code easier
+// This is simply a type alias to make reading the code easier
 type FrameIndex = int
 
-//A frame contains all the information needed to analyze the current place in the program
+// A frame contains all the information needed to analyze the current place in the program
 //
-//A subframe is a frame which shares the same `location` but has a different `name`
-//A call is a frame which has a different `location` while this frame instance 
+// A subframe is a frame which shares the same `location` but has a different `name`
+// A call is a frame which has a different `location` while this frame instance
 // is on the top of the stack
 //
-//When a frame is on the top of the stack, and a new function creates a frame,
+// When a frame is on the top of the stack, and a new function creates a frame,
 // that funtion's frame is added here as a `Call`
-type Frame struct{
-    Location    string          `msgpack:"location"`
-    Name        string          `msgpack:"name"`
-    Args        map[string]any  `msgpack:"args"`
-    Start       int             `msgpack:"start"`
-    End         int             `msgpack:"end"`
-    Duration    int             `msgpack:"duration"`
-    Calls       []FrameIndex    `msgpack:"calls"`
-    Subframes   []FrameIndex    `msgpack:"subframes"`
+type Frame struct {
+	Location  string         `msgpack:"location"`
+	Name      string         `msgpack:"name"`
+	Args      map[string]any `msgpack:"args"`
+	Start     int            `msgpack:"start"`
+	End       int            `msgpack:"end"`
+	Duration  int            `msgpack:"duration"`
+	Calls     []FrameIndex   `msgpack:"calls"`
+	Subframes []FrameIndex   `msgpack:"subframes"`
 }
